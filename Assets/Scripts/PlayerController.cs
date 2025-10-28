ba20d9f0f8a605e11a5822907ef05abdda989e2d
Jeswee <Jessica.Wiecha@Student.HTW-Berlin.de>
2025-10-28 15:30:38 +0100

    one-way-collider workaround
    Player Box Collider disabled when jumping upwards

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,27 +1,52 @@
+//using System.Numerics;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]     // ensures rigidbody2d always exists
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _jumpForce = 10f;
 
     private Rigidbody rb;
+    BoxCollider box;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        box = GetComponent<BoxCollider>();
     }
 
 
     private void OnCollisionEnter(Collision other)
     {
-        rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);       
+        // if not flagged
+        rb.linearVelocity = Vector3.zero;
+        rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        Debug.Log("Me is colliding!!!!");
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        // remove flag
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    void FixedUpdate()
+    {
+        if (rb.linearVelocity.y <= 0)
+        {
+            box.enabled = true;
+        }
+        else
+        {
+            box.enabled = false;
+        }
+        Debug.Log(rb.linearVelocity.y);
     }
 
 }

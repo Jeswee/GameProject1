b91a77d96c16538e4c17236c52b1c32c13e5d1b0
Jeswee <Jessica.Wiecha@Student.HTW-Berlin.de>
2025-11-04 12:45:12 +0100

    Game Zone

---
+++ Assets/Scripts/GameZone.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class GameZone : MonoBehaviour
+{
+    Camera cam;
+
+    void Awake()
+    {
+        cam = FindFirstObjectByType<Camera>();
+        if (cam == null)
+        {
+            Debug.LogError("No camera found!");
+            return;
+        }
+
+        SetToCameraSize();
+    }
+
+    void SetToCameraSize()
+    {
+        //Höhe in Welt-Einheiten
+        float height = cam.orthographicSize * 2f;
+        //Seitenverhältnis
+        float width = height * cam.aspect;
+
+        //Scale anpassen (weil Plane 10x10)
+        transform.localScale = new Vector3(width / 10f, 1f, height / 10f);
+    }
+}
